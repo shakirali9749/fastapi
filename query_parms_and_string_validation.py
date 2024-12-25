801f9570847b750782abca046361ed5f9acbe7e8
Shakir Ali <shakirali9749@gmail.com>
2024-12-25 17:04:49 +0500

    validation for query string as max_length, min_length and default value etc.

---
+++ query_parms_and_string_validation.py
@@ -0,0 +1,14 @@
+from typing import Annotated
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+@app.get("/items")
+async def read_item(
+	q: Annotated[ str | None, Query(min_length=4, max_length=50)] = None 
+	):
+	results = {"items":[{"item_id": "Foo"},{"item_id": "Bar"}]}
+	if q:
+		results.update({'q': q})
+	# breakpoint()
+	return results
