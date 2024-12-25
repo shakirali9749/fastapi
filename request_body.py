bd2f7797254cdd55e08037e745de3c0a166c05c2
Shakir Ali <shakirali9749@gmail.com>
2024-12-25 15:54:09 +0500

    Request body in fastapi using post and put metod

---
+++ request_body.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+	name: str
+	description: str | None = None
+	price: float
+	tex: float | None = None
+
+app = FastAPI()
+
+
+@app.post('/items/')
+async def create_item(item: Item):
+	item_dict = item.dict()
+	if item.tex:
+		price_with_tex = item.price + item.tex
+		item_dict.update({"price_with_tax": price_with_tex})
+	return item_dict
+
+
+@app.put('/items/{item_id}')
+async def update_item(item_id: int, item: Item, q: str | None = None):
+	result = {"item_id": item_id, **item.dict()}
+	if q:
+		result.update({"q":q})
+	return result
+
+
+
