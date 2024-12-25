9e5ca93b56cf524c0bc2713cf23fc4b860fd22f0
Shakir Ali <shakirali9749@gmail.com>
2024-12-25 19:08:12 +0500

    These all are are the validation of string

--- query_parms_and_string_validation.py
+++ query_parms_and_string_validation.py
@@ -5,9 +5,34 @@ from fastapi import FastAPI, Query
 app = FastAPI()
 
 
+# @app.get("/items/")
+# async def read_items(
+# 	q: Annotated[str | None, Query(title='quer string',min_length=3)] = None):
+#     result = {"items": [{"item_id": "foo"}, {"item_id": "Bar"}]}
+#     if q:
+#     	result.update({"q": q})
+# 	return result
+
+# alias is just like nick name of q query from the queqest
 @app.get("/items/")
-async def read_items(q: Annotated [list[str] | None, Query()] = None ):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+async def read_items(
+	q: Annotated[str | None, Query(alias="item-query")] = None):
+    result = {"items": [{"item_id": "foo"}, {"item_id": "Bar"}]}
     if q:
-        results.update({"q": q})
-    return results
+    	result.update({"q": q})
+
+    return result
+
+
+
+@app.get("/hidden_query/")
+async def read_items(
+    hidden_query: Annotated[str | None, Query(include_in_schema=False)] = None,
+):
+    if hidden_query:
+        return {"hidden_query": hidden_query}
+    else:
+        return {"hidden_query": "Not found"}
+
+
+
