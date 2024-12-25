49d42dd85f90922f9d0fe68166e887bcb612e76a
Shakir Ali <shakirali9749@gmail.com>
2024-12-25 17:20:53 +0500

    using this regex='^fixedquery$' in Query for validation

--- query_parms_and_string_validation.py
+++ query_parms_and_string_validation.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/items")
 async def read_item(
-	q: Annotated[ str | None, Query(min_length=4, max_length=50)] = None 
+	q: Annotated[ str | None, Query(min_length=4, max_length=50, regex='^fixedquery$')] = None 
 	):
 	results = {"items":[{"item_id": "Foo"},{"item_id": "Bar"}]}
 	if q:
