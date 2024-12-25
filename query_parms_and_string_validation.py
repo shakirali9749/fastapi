bcf05702af77f0605b5a0505bfcc2d5df3c8c703
Shakir Ali <shakirali9749@gmail.com>
2024-12-25 18:13:49 +0500

    Must required value of q from clients, even it will None there no issue

--- query_parms_and_string_validation.py
+++ query_parms_and_string_validation.py
@@ -1,14 +1,13 @@
 from typing import Annotated
+
 from fastapi import FastAPI, Query
 
 app = FastAPI()
 
-@app.get("/items")
-async def read_item(
-	q: Annotated[ str, Query(min_length=4)] 
-	):
-	results = {"items":[{"item_id": "Foo"},{"item_id": "Bar"}]}
-	if q:
-		results.update({'q': q})
-	# breakpoint()
-	return results
+
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Query(min_length=3)] = ...):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
