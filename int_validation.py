f254049ef16201c212167c26a9be638e4c33a67b
Shakir Ali <shakirali9749@gmail.com>
2024-12-26 14:23:15 +0500

    add constraints on path parms greater than or eqal to 1 ad ge=1

--- int_validation.py
+++ int_validation.py
@@ -6,9 +6,11 @@ app = FastAPI()
 
 @app.get("/items/{item_id}")
 async def read_items(
-	*, item_id: int = Path(title="The ID of the item to get"), q:str = Query(alias="item-query")
+	*, item_id: int = Path(title="The ID of the item to get",ge=1), 
+	q:str
 ):
 	results = {'item_id': item_id}
+	print(f'>>>>>>>>>>>>>>>>>>>>>> : {q}')
 	if q:
 		results.update({'q':q})
 	return results
