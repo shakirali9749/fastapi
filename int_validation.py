b8b0fda2bfee23885b31e850d3994c9d0d273a16
Shakir Ali <shakirali9749@gmail.com>
2024-12-26 14:43:19 +0500

    gt and lt are validations for float as greater than and less than

--- int_validation.py
+++ int_validation.py
@@ -6,11 +6,15 @@ app = FastAPI()
 
 @app.get("/items/{item_id}")
 async def read_items(
-	*, item_id: int = Path(title="The ID of the item to get",ge=0, le=100), 
-	q:str
+	*, item_id: int = Path(title="The ID of the item to get",ge=1, le=100), 
+	q:str,
+	size: Annotated[float,Query(gt=0,lt=10.5)]
 ):
 	results = {'item_id': item_id}
 	print(f'>>>>>>>>>>>>>>>>>>>>>> : {q}')
 	if q:
 		results.update({'q':q})
+
+	if size:
+		results.update({"size": size})
 	return results
